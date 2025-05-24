3c93c5aa93218b4c57b84c21769ce3b7046652f4
zoli0829 <zoli0829@gmail.com>
2025-05-24 11:02:32 +0100

    finished day 86

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello")
+            Spacer()
+                .frame(width: 100, height: 100)
+            Text("World")
         }
-        .padding()
+        .contentShape(.rect)
+        .onTapGesture {
+            print("VStack tapped!")
+        }
     }
 }
 
