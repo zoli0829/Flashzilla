9a749452f8cf314ea701569ff98e50dc59096381
zoli0829 <zoli0829@gmail.com>
2025-05-24 10:34:49 +0100

    Initial commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Zoltan Vegh on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
