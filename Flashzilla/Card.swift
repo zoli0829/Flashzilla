79f3122dd8430d50a6d857f1d34cf3d37815d70c
zoli0829 <zoli0829@gmail.com>
2025-05-26 11:32:57 +0100

    finished day 88

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Zoltan Vegh on 26/05/2025.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jody Whittaker")
+}
